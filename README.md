1b5dc444312995fb3d08b8d4fe4ea488fa87d93a
Rudie G <rauoldm@gmail.com>
2021-05-02 23:12:35 -0500

    Trying to fix bugs while very ill, didn't go well

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# 04-homework-web-apis
-A code quiz application
+# HOMEWORK 4: WEB APIs
+
+## Description:
+    A code quiz application, the user clicks buttons to answer questions. A time penalty occurs if the user answers incorrectly, and the user's score increases if they answer correctly
+    At the end of the quiz, the scores of recent attempts are displayed with the names of the users
+
+## Intended Functionality:
+    The user clicks on the Begin Quiz button and the HTML elements that display the quiz are displayed and filled with text from objects in an array of questions. When the user clicks the right answer, their score increases, but they are not notified. When the user selects incorrectly, a time penalty occurs, which compounds if they continue to answer incorrectly
+
+## Current Issues:
+    The end screen with the scores does not appear (is not coded yet), and the part of the application that takes in the user's name and stores it with the score in localStorage is not functional
--- assets/scripts.js
+++ assets/scripts.js
@@ -21,9 +21,9 @@ var questions = [
         "correctOp": "third-party API"
     },
     {
-        "question": "In layman's terms, what does the kernel do?",
-        "wrongOps": ["turns into popcorn","moderates data","moderates power", "initializes software"],
-        "correctOp": "manages hard/software"
+        "question": "What kind of design has become the norm for developers in recent years?",
+        "wrongOps": ["pleasing","post-goth","user-first", "efficient"],
+        "correctOp": "mobile-first"
     }
 ];
 var body = document.body;
@@ -57,8 +57,6 @@ function countdown() {
     }
     }, 1000);
 }
-var ulEl = document.createElement("ul")
-var liEl = document.createElement("il")
 player = "";
 
 // Hiding the question and answer fields, as well as the timer
@@ -83,15 +81,24 @@ function theQuiz() {
         hidded[i].style.display = "flex";
     }
     // Start countdown
-    countdown();
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = "";
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+        }
+        }, 1000);
     // Hide start button
     startButton.style.display = "none";
     // Shuffle the array with the question objects
     shuffleArray(questions);
     theQuestionInvoker();
-    if (timeLeft <= 0) {
-        theEnd();
-    }
 }
 
 function theQuestionInvoker () {
@@ -121,10 +128,12 @@ function theQuestionInvoker () {
                     timeLeft -= 5;
                 }
                 theQuestionInvoker();
-                // TODO: Get the theEnd function to run after either the timeLeft reaches zero or the user answers all questions
             });
-        } 
+        }
     }
+    else if (timeLeft <= 0) {
+        theEnd();
+    } 
 }
 tempScores = [];
 localStorage.setItem("scores", tempScores);
@@ -135,7 +144,7 @@ function theEnd() {
     var player = prompt("What's your name (three letters only)?")
     player = 
     endScore = score + " " + player;
-    var storedScores = JSON.parse(localStorage.getItem("scores"));
+    var storedScores = localStorage.getItem("scores");
     if (storedScores == null) {
         storedScores = [];
     }
